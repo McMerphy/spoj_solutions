4bc511836970ba885e202e220cfc4d5fe1b66573
aleksei <marozaliaksei@gmail.com>
2015-04-25 18:34:50 +0300

    4 Transform the Expression

--- 3.reverce_polish_notation/rpn.rb
+++ 3.reverce_polish_notation/rpn.rb
@@ -1,18 +1,32 @@
 $stdin = File.open('input_rpn.txt')
 
 n = gets.to_i
+operations = ['+', '-', '*', '/', '^']
 
 while n > 0 do
   line = gets.split("")
-  level = 0
-  data = []
-  i = 1
+  operators = []
+  output = ""
+  i = 0
   while i < line.length do
-    level =+ 1 if line[i] == '('
-    if line[i] == ')'
-      puts ')'
+    if operations.include? line[i] 
+      operators << line[i]
+      i += 1
+      next
     end
+    if line[i] == ')'
+      output += operators.pop
+      i += 1
+      next
+    end
+    if line[i] == '('
+      i += 1
+      next
+    end
+    output += line[i]
     i += 1
   end
+  puts output
   n -= 1
 end
+
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Passing solutions:
 - 1.  Life, the Universe, and Everything   
 - 2.  Prime Generator  
+  4.  Transform the Expression
